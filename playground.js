09a456ac419dc250239428f037266106b974a39d
benicio.januario <benicio.januario@ifood.com.br>
2020-04-25 18:36:17 -0300

    Remove happy face

--- playground.js
+++ playground.js
@@ -1,4 +1,4 @@
 // esse e um codigo novo de js que eu escrevi localmente e preciso que outras pessoas do meu time aprovem
 
 
-console.log("oi eu sou uma alteração!!! :)");
+console.log("oi eu sou uma alteração!!!");
