ca77f9a6ee1800585b6ea4f471fa91bd8481f35a
benicio.januario <benicio.januario@ifood.com.br>
2020-04-28 15:30:24 -0300

    Unreverting last commit

---
+++ backend/account/AccountService.js
@@ -0,0 +1,31 @@
+const firebase = require('firebase');
+require('firebase/firestore');
+
+let accountService = class {
+    constructor() {
+        this.db = firebase.firestore();
+    }
+
+    getAccountById = (UID) => this.db.collection('accounts').doc(UID).get();
+
+    getAccountByEmail = (email) => this.db.collection('accounts').where('email', '==', email).get();
+
+    getAccountByNickname = (nickname) => this.db.collection('accounts').where('nickname', '==', nickname).get();
+    
+    deleteAccountById = (UID) => this.db.collection('accounts').doc(UID).delete();
+    
+    createAccount = (nickname, email, avatar, age) => {
+        let accountUID = this.db.collection('accounts').doc().id;
+        return {
+            accountCreationPromise: this.db.collection('accounts').doc(accountUID).set({ nickname, email, avatar, age }),
+            createdAccountUID: accountUID
+        }
+    }
+    
+    updateAccountById = (UUID, {nickname, email, avatar, age}) => this.db.collection('accounts').doc(UUID).set({ nickname, email, avatar, age })
+
+    partiallyUpdateAccountById = (UUID, reqBody) => this.db.collection('accounts').doc(UUID).set(reqBody, {merge: true})
+}
+    
+
+module.exports = new accountService();
